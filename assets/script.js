94585f41aa72b41f08305165a383b811c4823a7b
rudevenger <srfromadhon@gmail.com>
2021-05-06 10:25:35 +0700

    add eventlistener for playing bizarre-piano with keyboard

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,22 @@
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
+
 const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.white-key')
+const blackKeys = document.querySelectorAll('.key.black-key')
 
 keys.forEach(key => {
     key.addEventListener('click', () => playNote(key))
+})
+
+document.addEventListener('keydown', e => {
+    if (e.repeat) return
+    const key = e.key
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+    const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+    if(whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+    if(blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
 })
 
 function playNote(key) {
